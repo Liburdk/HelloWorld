44a20ff1400497ee0204017d15cd41757ebe7a51
Kaley Liburd <Liburd.k@yahoo.com>
2020-01-23 12:59:34 -0500

    Version 2

--- Program.cs
+++ Program.cs
@@ -7,7 +7,9 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             // Pause to display "Hello World!"
+            Console.WriteLine("Press any key to continue...");
             Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
             
         }
     }
